0fb013a64d9d3b6142012426217c4f814214c5a1
Divyanshu Dwivedi <divyanshudwivedi1290@gmail.com>
2025-09-01 17:28:37 +0530

    Complete Project

--- Backend/server.js
+++ Backend/server.js
@@ -2,16 +2,30 @@ import express from "express";
 import dotenv from "dotenv";
 import { connectDB } from "./config/db.js";
 import productRoutes from "./routes/product.route.js";
+import path from "path";
 
 dotenv.config();
 
 const app = express();
 
-app.use(express.json());
-// allows us to accept JSON data in req.body
+app.use(express.json()); // allows us to accept JSON data in req.body
+const __dirname = path.resolve();
 
+//  CRITICAL: API routes MUST come first!
 app.use("/api/products", productRoutes);
+
+if (process.env.NODE_ENV === "production") {
+    // Serve static files from dist folder
+    app.use(express.static(path.join(__dirname, "/Frontend/dist")));
+
+    // Catch-all handler: send back React's index.html file for any non-API routes
+    app.get("*", (req, res) => {
+        res.sendFile(path.resolve(__dirname, "Frontend", "dist", "index.html"));
+    });
+}
+
 const port = process.env.PORT || 5000;
+
 connectDB()
     .then(() => {
         app.listen(port, () => {
